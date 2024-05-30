90cc9cd22d98db747470269f3b09fa29a4e68f7c
plite1499 <plite1499@gmail.com>
2024-05-30 22:48:55 +0900

    clip修正

---
+++ src/app/clips/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "clips",
+  description: "clips",
+};
+
+const clips = () => {
+  return (
+    <>
+      <div>aaaaaaa</div>
+    </>
+  );
+};
+
+export default clips;
